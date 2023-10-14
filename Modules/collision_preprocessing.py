4ed38e058cd3f395bb44a221901ca331aad930ac
JP <jpniznhy@hotmail.com>
2023-10-14 12:36:59 +0200

    modules

--- Modules/collision_preprocessing.py
+++ Modules/collision_preprocessing.py
@@ -18,12 +18,13 @@ import os
 import numpy as np
 import pandas as pd
 
-exists_pre = os.path.isfile('data/collision_clean.csv')
+# revisar esta condicion
+exists_pre = os.path.isfile('Data/collision_clean.csv')
 
 ####################################################################################################
 # FUNCTIONS ############################################################################ FUNCTIONS #
 ####################################################################################################
-def imputation_with_ref_col(dataset: pd.DataFrame, imputed_col: str, reference_col: str, imputed_value: str, begin=1, end=1) -> None:
+def imputation_with_ref_col(dataset: pd.DataFrame, imputed_col: str, reference_col: str, imputed_value: str) -> None:
     """
     This function imputes the values of a column with the values of another column as reference.
 
@@ -37,13 +38,8 @@ def imputation_with_ref_col(dataset: pd.DataFrame, imputed_col: str, reference_c
         The column to be used as reference.
     imputed_value : str
         The value to be imputed.
-    begin : int
-        The first column to be imputed if there is a range of columns.
-    end : int
-        The last column to be imputed if there is a range of columns.
     """
     if exists_pre:
         return
 
-    for i in range(begin, end+1):
-        dataset[imputed_col + f' {i}'] = dataset.apply(lambda x: imputed_value if not pd.isnull(x[reference_col + f' {i}']) and pd.isnull(x[imputed_col + f' {i}']) else x[imputed_col + f' {i}'], axis=1)
+    dataset[imputed_col] = dataset.apply(lambda x: imputed_value if not pd.isnull(x[reference_col]) and pd.isnull(x[imputed_col]) else x[imputed_col], axis=1)
